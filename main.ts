17094ceb22d8cfe76648894647861b3687374aa2
irelandmcginnis <111801748+irelandmcginnis@users.noreply.github.com>
2022-08-24 12:19:53 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Angry)
+    basic.pause(200)
+    basic.showIcon(IconNames.Asleep)
+    basic.pause(200)
+    basic.showIcon(IconNames.Surprised)
+})
