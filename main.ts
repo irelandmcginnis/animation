867fb28d76c7e6720bef5b3ad1123399397b42d0
irelandmcginnis <111801748+irelandmcginnis@users.noreply.github.com>
2022-08-29 12:17:42 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+let time = 0
+let strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
+strip.setBrightness(100)
+strip.showRainbow(200, 360)
 basic.forever(function () {
-    basic.showIcon(IconNames.Angry)
-    basic.pause(200)
-    basic.showIcon(IconNames.Asleep)
-    basic.pause(200)
-    basic.showIcon(IconNames.Surprised)
+    strip.shift(1)
+    time = 5
+    strip.rotate(2)
+    strip.show()
+    for (let index = 0; index <= 10; index++) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Indigo))
+    }
 })
